c31381c4e604316dff99df847eb22a4f2633a94d
Diana <diana_ivanivna@kr.net>
2020-04-30 18:26:53 +0300

    Updates

---
+++ precache-manifest.fd59bde7d8ee33e6f63404d6a3ebb859.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b2e190ed207925e8912786a5d36f81",
+    "url": "/styled-component-react/index.html"
+  },
+  {
+    "revision": "ab427508f50a4783722a",
+    "url": "/styled-component-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "202a103a3b85a8cabe32",
+    "url": "/styled-component-react/static/js/2.903f9246.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/styled-component-react/static/js/2.903f9246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab427508f50a4783722a",
+    "url": "/styled-component-react/static/js/main.153c6419.chunk.js"
+  },
+  {
+    "revision": "eed2f40dbad8f228d02c",
+    "url": "/styled-component-react/static/js/runtime-main.7fecd363.js"
+  }
+]);
